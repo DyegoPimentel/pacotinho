d11704364da12597e231e276a89fae4e7357539d
Dyego Pimentel <dyegobusiness@gmail.com>
2024-11-09 12:41:46 -0400

    live 2: https://youtube.com/live/QWPtv9iApqs

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { FeedComponent } from './pages/feed/feed.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'feed'
+    },
+    {
+        path: 'feed',
+        component: FeedComponent
+    },
+    {
+        path: '**',
+        redirectTo: 'feed'
+    },
+];
